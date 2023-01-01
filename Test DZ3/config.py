e1984b9c9b74d7b64375564b838785b7a3b5c88d
vompaire <vompaire@mail.ru>
2023-01-01 19:53:33 +0300

    end
    end

---
+++ Test DZ3/config.py
@@ -0,0 +1,12 @@
+import telebot
+bot = telebot.TeleBot("5735677215:AAHwrY0jB11milxJ_OhAMnYrewc4Dg0acP8")
+
+keys = {
+    "рубль": "RUB",
+    "евро": "EUR",
+    "доллар": "USD",
+}
+payload = {}
+headers = {
+    "apikey": "cMY2tJY3aD81jj95EpYGddvSq4QIed5p"
+}
